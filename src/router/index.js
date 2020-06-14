c2a90d851d5c8ae3d8ec608beee21ac9840e04e7
iRainy6661 <JianK6661@163.com>
2020-06-14 19:55:40 +0800

    代码优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 // import Vue from 'vue'
 // import Router from 'vue-router'
-// import HelloWorld from '@/components/HelloWorld'
 
 // Vue.use(VueRouter)
 
@@ -30,6 +29,5 @@ export default new VueRouter({
       name: 'loadIcon',
       component: resolve => require(['@/view/component/loadIcon'], resolve),
     }
-
   ]
 })
