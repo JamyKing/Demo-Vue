23c13f5426f567f47c7e13674d673cbebca8cd46
jujiankang <jiank6661@163.com>
2020-06-28 15:17:44 +0800

    loadIcon修改

--- src/view/component/loadIcon.vue
+++ src/view/component/loadIcon.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="u-f-col">
         <h2>动态加载JS/CSS文件</h2>
-        <h3>Icon使用阿里矢量图标，分别采用symbol引用、class引用，可查看dom树，只是因为没有引入对应的js、css，icon图标无法显示。</h3>
+        <h3>Icon使用阿里矢量图标，这里使用symbol引用，可查看dom树，只是因为没有引入对应的js文件，icon图标无法显示。(css加载这里就不单独演示了)</h3>
         <el-row :gutter="30">
             <el-col :span="8">
                 <el-card class="card">
-                    <h4>symbol引用</h4>
+                    <h4 style="margin-bottom: 10px;">symbol引用</h4>
                     <el-tag class="tags">
                         <svg class="icon" aria-hidden="true">
                             <use xlink:href="#icon-link"></use>
@@ -32,28 +32,7 @@
                         </svg>
                     </el-tag>
                 </el-card>
-                <el-button plain :disabled="overJS" @click="load(true)" v-loading.fullscreen.lock="Loading">加载JS</el-button>
-            </el-col>
-            <el-col :span="8">
-                <el-card class="card">
-                    <h4>class引用</h4>
-                    <el-tag class="tags">
-                        <i class="iconfont icon-link" style="font-size: 20px;"></i>
-                    </el-tag>
-                    <el-tag class="tags" type="success">
-                        <i class="iconfont icon-github" style="font-size: 20px;"></i>
-                    </el-tag>
-                    <el-tag class="tags" type="info">
-                        <i class="iconfont icon-zhihu" style="font-size: 20px;"></i>
-                    </el-tag>
-                    <el-tag class="tags" type="warning">
-                        <i class="iconfont icon-jianshu" style="font-size: 20px;"></i>
-                    </el-tag>
-                    <el-tag class="tags" type="danger">
-                        <i class="iconfont icon-uniapp" style="font-size: 20px;"></i>
-                    </el-tag>
-                </el-card>
-                <el-button plain :disabled="overCSS" @click="load(false)" v-loading.fullscreen.lock="Loading">加载CSS</el-button>
+                <el-button type="primary" plain :disabled="overJS" @click="load(true)" v-loading.fullscreen.lock="Loading">加载JS</el-button>
             </el-col>
         </el-row>
         <h3>加载时可打开浏览器调试工具，Network中查看。</h3>
@@ -73,31 +52,24 @@ export default {
             loadTypeJs: true,
             loadUrl: null,
             overJS: false,
-            overCSS: false,
             Loading: false
         }
     },
     methods: {
-        load (val) {
+        load () {
             this.Loading = true
             setTimeout(() => {
-                this.loadTypeJs = val
-                this.loadUrl = `https://at.alicdn.com/t/font_1861818_uturk9fjhcd.${val ? 'js' : 'css'}`
+                this.loadUrl = 'https://at.alicdn.com/t/font_1861818_uturk9fjhcd.js'
             }, 1000)
         },
         loadOver () {
             // 当使用远程js里的内容时请添加"//eslint-disable-line"防止eslint检测报错
             // eslint-disable-line
-            const { loadTypeJs } = this
             this.Loading = false
-            if (loadTypeJs) {
-                this.overJS = true
-            } else {
-                this.overCSS = true
-            }
+            this.overJS = true
             this.$message({
                 showClose: true,
-                message: `IconFont远程【${loadTypeJs ? 'JS' : 'CSS'}】加载完毕！`,
+                message: 'IconFont远程【JS】加载完毕！',
                 type: 'success'
             })
         }
