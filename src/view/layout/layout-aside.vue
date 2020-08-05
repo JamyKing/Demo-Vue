bb3e110e4035b6ccd7069a13035df898219bda0c
Rainy <JianK6661@163.com>
2020-08-05 11:09:47 +0800

    优化布局计算

--- src/view/layout/layout-aside.vue
+++ src/view/layout/layout-aside.vue
@@ -1,27 +1,29 @@
 <template>
     <el-menu
-        :default-active="$route.path"
         router
+        :default-active="$route.path"
         background-color="#545c64"
         text-color="#fff"
         active-text-color="#ffd04b"
         :style="getHeight">
-        <el-menu-item :index="item.path" v-for="item in noChildren" :key="item.path">
-            <i :class="'el-icon-' + item.icon"></i>
-            <span slot="title">{{ item.label }}</span>
-        </el-menu-item>
-        <el-submenu :index="item.label" v-for="(item, index) in hasChildren" :key="index">
-            <template slot="title">
+        <div v-for="(item, index) in menu" :key="index">
+            <el-menu-item v-if="item.name" :index="item.path">
                 <i :class="'el-icon-' + item.icon"></i>
                 <span slot="title">{{ item.label }}</span>
-            </template>
-            <el-menu-item-group>
-                <el-menu-item :index="subItem.path" v-for="(subItem, subIndex) in item.children" :key="subIndex">
-                    <i :class="'el-icon-' + subItem.icon"></i>
-                    <span slot="title">{{ subItem.label }}</span>
-                </el-menu-item>
-            </el-menu-item-group>
-        </el-submenu>
+            </el-menu-item>
+            <el-submenu v-else :index="item.label">
+                <template slot="title">
+                    <i :class="'el-icon-' + item.icon"></i>
+                    <span slot="title">{{ item.label }}</span>
+                </template>
+                <el-menu-item-group>
+                    <el-menu-item :index="subItem.path" v-for="(subItem, subIndex) in item.children" :key="subIndex">
+                        <i :class="'el-icon-' + subItem.icon"></i>
+                        <span slot="title">{{ subItem.label }}</span>
+                    </el-menu-item>
+                </el-menu-item-group>
+            </el-submenu>
+        </div>
     </el-menu>
 </template>
 
@@ -87,14 +89,6 @@ export default {
         getHeight () {
             let height = this.$store.state.clientHeight
             return `height: ${height}px;`
-        },
-        //没有子菜单
-        noChildren() {
-            return this.menu.filter(item => !item.children)
-        },
-        //有子菜单 (这样设置会有一个问题 就是有子菜单的 永远会在没有子菜单的下面）
-        hasChildren() {
-            return this.menu.filter(item => item.children)
         }
     },
     methods: {
